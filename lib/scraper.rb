65627be43a7606ae79773879dcd3a46c42508cd8
Tom Banish <tombanish@gmail.com>
2020-04-27 16:13:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    Nokogiri::HTML(open)
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
   end
   
